fafead22800a4580f3be8303e999837fd072ce67
Luca Valente <luca.valente2@unibo.it>
2021-03-01 13:07:10 +0100

    mchan test update

--- mchan_tests/testMCHAN_TCDM2TCDM_tx_rx/mchan_tests.h
+++ mchan_tests/testMCHAN_TCDM2TCDM_tx_rx/mchan_tests.h
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2018 ETH Zurich and University of Bologna
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/* 
- * Mantainer: Luca Valente (luca.valente2@unibo.it)
- */
-
 #ifndef MCHAN_TEST_H
 #define MCHAN_TEST_H
 
@@ -30,8 +10,8 @@
 #define LIN 0
 #define TWD 1
 
-#define MCHAN_COMMAND_QUEUE    0x10201800
-#define MCHAN_STATUS_REGISTER  0x10201804
+#define MCHAN_COMMAND_QUEUE    0x10204400 //0x10201800
+#define MCHAN_STATUS_REGISTER  0x10204404 //0x10201804
 
 // TEMPORARY FIX DAVIDE
 #define PLP_DMA_2D_TCDM_BIT 22
