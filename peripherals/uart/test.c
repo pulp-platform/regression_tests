e0c3cbc1b9ec9ba960435f771bf74454e46cf227
Corrado Bonfanti <bonfantic@lagrev5.ee.ethz.ch>
2021-07-25 20:02:59 +0200

    tests/runtime-tests: Change the number of tests to run on UART peripheral.

--- peripherals/uart/test.c
+++ peripherals/uart/test.c
@@ -59,7 +59,7 @@ int error = 0;
 int tx_buffer[BUFFER_SIZE] = {'S','t','a','y',' ','a','t',' ','h','o','m','e','!','!','!','!'};
 int rx_buffer[BUFFER_SIZE];
 
-for (int u = 0; u < 2; ++u)
+for (int u = 0; u < 1; ++u)
 {
   for (int j = 0; j < BUFFER_SIZE; ++j)
   {
