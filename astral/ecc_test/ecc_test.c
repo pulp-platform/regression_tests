126a1c56a6774d235f52b05ea1e3d5566606effd
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-03-23 20:14:51 +0100

    Add astral target.

---
+++ astral/ecc_test/ecc_test.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2018 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <pulp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 0x400
+#define NUM_BANKS 16
+#define SCRUBBER_INTERVAL 2
+
+int main() {
+  // Collecting info about the core ID and the running cluster ID
+  unsigned int core_id = get_core_id();
+  unsigned int cluster_id = rt_cluster_id();
+
+  if (rt_cluster_id() != 0) return bench_cluster_forward(0);
+
+  if (core_id != 0) synch_barrier();
+
+  unsigned int *test_array = pi_l1_malloc(cluster_id, SIZE);
+
+  // Initializing the memory
+  for (int i = 0; i < SIZE; i++) {
+    pulp_write32(&test_array[i], i);
+  }
+
+  // Initialize the scrubbing interval for all memory banks
+  for (int i = 0; i < NUM_BANKS; i++)
+    tcdm_scrubber_set_interval(cluster_id, i, SCRUBBER_INTERVAL);
+
+  // Initialize the error-tracking variables
+  bool  mismatch = 0;
+  unsigned int error = 0;
+  for (int i = 0; i < SIZE; i++) {
+    mismatch = (pulp_read32(&test_array[i]) != i);
+    if (mismatch) {
+      error ++;
+      printf("Expected 0x%x, got 0x%x\n", i, pulp_read32(&test_array[i]));
+    }
+  }
+
+  unsigned int mismatch_cnt = 0;
+  unsigned int fix_cnt = 0;
+  unsigned int uncorrectable_cnt = 0;
+  for (int i = 0; i < 16; i++) {
+    mismatch_cnt += tcdm_scrubber_get_mismatch_count(cluster_id, i);
+    fix_cnt += tcdm_scrubber_get_fix_count(cluster_id, i);
+    uncorrectable_cnt += tcdm_scrubber_get_uncorrectable_count(cluster_id, i);
+  }
+
+  printf("mismatch_cnt: %d, fix_cnt: %d, uncorrectable_cnt: %d\n", mismatch_cnt, fix_cnt, uncorrectable_cnt);
+
+  return (error != 0) ^ (uncorrectable_cnt != 0);
+}
